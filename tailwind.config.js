61b566baa8a52f8efee50923b7ef58c92fb665ac
cam96stanley <cam96stanley@gmail.com>
2024-11-25 18:56:19 -0500

    added font families and boxshadow to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bellefair: ["Bellefair", "serif"],
+        barlowCon: ["BarlowCondensed", "sans-serif"],
+        barlow: ["Barlow", "sans-serif"],
+      },
+      boxShadow: {
+        linkHover: "0px 0px 80px 50px rgba(255, 255, 255, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
